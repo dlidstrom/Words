7aaafca6046363e0bbfa8f10d08c8fabeaa349ea
Daniel Lidstrom <dlidstrom@gmail.com>
2018-07-16 07:02:40 +0200

    Preparing for server

--- Words.Web/Global.asax.cs
+++ Words.Web/Global.asax.cs
@@ -4,8 +4,6 @@
     using System.Globalization;
     using System.IO;
     using System.Text;
-    using System.Threading;
-    using System.Threading.Tasks;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Routing;
@@ -18,74 +16,24 @@
     // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : HttpApplication
     {
-#if DEBUG
-        private const bool IsDebugFlag = true;
-#else
-        private const bool IsDebugFlag = false;
-#endif
         private static readonly Logger Log = LogManager.GetCurrentClassLogger();
-        private static readonly AutoResetEvent LoadingEvent = new AutoResetEvent(false);
         private static WordFinder wordFinder;
         private static NianFinder nianFinder;
         private static IDocumentStore documentStore;
 
-        public static bool IsDebug { get { return IsDebugFlag; } }
+        public static WordFinder WordFinder => wordFinder;
 
-        public static WordFinder WordFinder
-        {
-            get
-            {
-                if (wordFinder == null && !LoadingEvent.WaitOne(20000))
-                    throw new Exception("Failed to load dictionary");
-                return wordFinder;
-            }
-        }
+        public static NianFinder NianFinder => nianFinder;
 
-        public static NianFinder NianFinder
-        {
-            get
-            {
-                if (nianFinder == null && !LoadingEvent.WaitOne(20000))
-                    throw new Exception("Failed to load nian");
-                return nianFinder;
-            }
-        }
+        public static IDocumentStore DocumentStore => documentStore;
 
-        public static IDocumentStore DocumentStore
-        {
-            get
-            {
-                lock (typeof(MvcApplication))
-                {
-                    if (documentStore == null)
-                    {
-                        Log.Info("Initializing document store");
-                        if (IsDebug)
-                            documentStore = new DocumentStore { ConnectionStringName = "RavenDB" };
-                        else
-                        {
-                            documentStore = new DocumentStore
-                            {
-                                ConnectionStringName = "RavenDB"
-                            };
-                        }
-
-                        documentStore.Initialize();
-                        Log.Info("Document store initialized");
-                    }
-
-                    return documentStore;
-                }
-            }
-        }
-
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        private static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new UserTrackerLogAttribute());
         }
 
-        public static void RegisterRoutes(RouteCollection routes)
+        private static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
@@ -103,7 +51,14 @@
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
             LoadDictionary();
-            Task.Factory.StartNew(() => DocumentStore);
+            Log.Info("Initializing document store");
+            documentStore = new DocumentStore
+            {
+                ConnectionStringName = "RavenDB"
+            };
+
+            documentStore.Initialize();
+            Log.Info("Document store initialized");
         }
 
         protected void Application_End()
@@ -114,17 +69,13 @@
 
         private static void LoadDictionary()
         {
-            Task.Factory.StartNew(() =>
-                {
-                    Log.Info("Loading dictionary");
-                    object dir = AppDomain.CurrentDomain.GetData("DataDirectory");
-                    string filename = Path.Combine(dir.ToString(), "words.txt");
-                    string language = Path.Combine(dir.ToString(), "nian.txt");
-                    wordFinder = new WordFinder(filename, Encoding.UTF8, Language.Swedish);
-                    nianFinder = new NianFinder(language, Encoding.UTF8, new CultureInfo("sv-SE"));
-                    LoadingEvent.Set();
-                    Log.Info("Dictionary loaded");
-                });
+            Log.Info("Loading dictionary");
+            var dir = AppDomain.CurrentDomain.GetData("DataDirectory");
+            var filename = Path.Combine(dir.ToString(), "words.txt");
+            var language = Path.Combine(dir.ToString(), "nian.txt");
+            wordFinder = new WordFinder(filename, Encoding.UTF8, Language.Swedish);
+            nianFinder = new NianFinder(language, Encoding.UTF8, new CultureInfo("sv-SE"));
+            Log.Info("Dictionary loaded");
         }
     }
 }
